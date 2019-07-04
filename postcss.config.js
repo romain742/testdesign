0856a314a5d1172b0be3c93703209f10dda113ab
Romain Perrot <romain742@gmail.com>
2019-07-04 10:35:38 +0700

    build:css

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("postcss-custom-properties"),
-    require("mqpacker")({ sort: true }),
-    require("cssnano")({
-      preset: ["advanced", { discardComments: { removeAll: true } }]
+    require("postcss-custom-properties")({
+      preserve: false
     }),
-    require("postcss-reporter")
+    require("postcss-csso"),
+    require("mqpacker")({ sort: true })
   ]
 };
