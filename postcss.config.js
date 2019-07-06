bc1234400c4222a0ada62fa22bd946d31e42b9d2
Terrence Tempest Wood <terrence@tempestwood.com>
2019-07-06 10:51:07 +0700

    Added fonts, added build process

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     require("postcss-import"),
+    require("postcss-custom-media"),
     require("postcss-custom-properties")({
       preserve: false
     }),
